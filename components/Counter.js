cd5d928ad9fb93c34350c537379ce6bde0d3f364
beau101023 <bbaer10000@gmail.com>
2024-11-10 20:43:33 -0700

    parameterize counter background and increment amount, reset counter at 10

--- components/Counter.js
+++ components/Counter.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
-  
-    return (
-      <div>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-      </div>
-    );
-  }
+export default function Counter({amt, color}) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button style={{backgroundColor: color}} onClick={() => {
+          var newCount = count+amt;
+          if(newCount>10) newCount = 0;
+          setCount(newCount);
+        }
+      }>Increment</button>
+    </div>
+  );
+}
